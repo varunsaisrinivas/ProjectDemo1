0a6b2ddde94826503e5fd92a9dd301374ad41b0e
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-08-17 09:41:11 +0530

    this is the first commit...

---
+++ src/sample/MyProject.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class MyProject {
+	public static void main(String args[])
+	{
+		String name="yes";
+		if (name=="yes") {
+			System.out.println("name is correct");
+		}
+		else
+		{
+			System.out.println("enter the correct name ");
+		}
+	}
+
+}
